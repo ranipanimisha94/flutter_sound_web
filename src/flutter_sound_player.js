07a4f68709d18e77b3d334e504920ba877a48293
larpoux <larpoux@gmail.com>
2025-01-28 16:45:17 +0100

    TAU : Version 9.20.3

--- src/flutter_sound_player.js
+++ src/flutter_sound_player.js
@@ -18,7 +18,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const PLAYER_VERSION = '9.20.2'
+const PLAYER_VERSION = '9.20.3'
 
 function newPlayerInstance(aCallback, callbackTable) { return new FlutterSoundPlayer(aCallback, callbackTable); }
 
