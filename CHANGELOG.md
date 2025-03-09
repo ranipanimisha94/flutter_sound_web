2cca80d797c4ac25a1b04a3dc026870ebe851815
larpoux <larpoux@gmail.com>
2025-03-09 17:32:18 +0100

    TAU : Version 9.25.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 9.25.1
+## 9.25.2
 
 - Please [look to this](https://flutter-sound.canardoux.xyz/changelog.html)
 
--- src/flutter_sound.js
+++ src/flutter_sound.js
@@ -18,7 +18,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const VERSION = '9.25.1'
+const VERSION = '9.25.2'
 
 
 const VERBOSE = 0;
